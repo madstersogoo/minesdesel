e4296c7e5b9819357a44491176527c62b080201f
Madstersogood <53706800+madstersogoo@users.noreply.github.com>
2020-05-13 16:20:52 +0200

    update

--- bot.py
+++ bot.py
@@ -7,6 +7,9 @@ client = commands.Bot(command_prefix = "!")
 
 userlist = []
 votelist = []
+memberlist = []
+count = 1
+salt = 100
 
 with open('userlist.txt', 'r') as file:
     userlist = [line.strip() for line in file]
@@ -14,25 +17,47 @@ with open('userlist.txt', 'r') as file:
 with open('vote.txt', 'r') as file:
     votelist = [line.strip() for line in file]
 
+with open('member.txt', 'r') as file:
+    memberlist = [line.strip() for line in file]
+
 
 @client.event
 async def on_ready():
     print('bot ready')
     await client.change_presence(status=discord.Status.idle, activity=discord.Game('mining salt'))
 
+
+
 @client.command(aliases=['votes'])
 async def vote(ctx, *, user):
-    if (user) in userlist:
+    if (ctx.author) in memberlist:
+        index = memberlist.index(ctx.author)
+        memberlist.pop(index)
+        votelist.pop(index)
         await ctx.send(f"Et bim ! %s prend un vote" % user)
         votelist.append(user)
+        memberlist.append(ctx.author)
         with open('vote.txt', 'w') as f:
             for item in votelist:
                 f.write("%s\n" % item)
-        print (votelist)
+        with open('member.txt', 'w') as f:
+            for item in memberlist:
+                f.write("%s\n" % item)
     else :
-            await ctx.send(f"you can't vote against this user")
-        
-
+        if (user) in userlist:
+                await ctx.send(f"Et bim ! %s prend un vote" % user)
+                votelist.append(user)
+                memberlist.append(ctx.author)
+                with open('vote.txt', 'w') as f:
+                    for item in votelist:
+                        f.write("%s\n" % item)
+                with open('member.txt', 'w') as f:
+                    for item in memberlist:
+                        f.write("%s\n" % item)
+                print (votelist)
+        else :
+                    await ctx.send(f"you can't vote against this user")
+                
 
 @client.command()
 async def db(ctx):
@@ -61,6 +86,14 @@ async def useradd(ctx, *, user):
 async def userslist(ctx):
     await ctx.send(userlist)
     print (userlist)
+
+@client.command()
+async def loubier(ctx):
+    await ctx.send(f'@everyone fuck le francais')
+
+@client.command()
+async def salt(ctx):
+    await ctx.send(f'100')
 
 
 
